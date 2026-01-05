053507aa374e4792ccb333207e9ff920ee23c23c
pratik mungase <pratikmungase7385@gmail.com>
2026-01-05 21:57:53 +0530

    fix db env usage

--- api.py
+++ api.py
@@ -148,15 +148,14 @@ templates = Jinja2Templates(directory="templates")
 # )
 
 
-
 import os
 import pymysql
 
 db = pymysql.connect(
-    host=os.getenv("localhost"),
-    user=os.getenv("root"),
-    password=os.getenv("mysql"),
-    database=os.getenv("ml"),
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME"),
     port=int(os.getenv("DB_PORT", 3306)),
     cursorclass=pymysql.cursors.DictCursor,
     autocommit=True
